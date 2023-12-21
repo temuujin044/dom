d66d20832ccc8d1bcc3f10555515b464b96af281
Temuujin undefined <23LP5268@G401-CC0096.local>
2023-12-21 14:27:14 +0800

    secondTest

--- game.js
+++ game.js
@@ -6,12 +6,36 @@ function initial() {
   let startButton = document.createElement("button");
   startButton.innerHTML = "Start";
   tutorial.innerText =
-    "start darhad togloom ehlene \n\n ongonuud garch irne tednees neg ondoog ni songoj darna \n\n 60 secondiin hugatsaand ali boloh olon onoo tsugluulah heregtei";
-  startDiv.setAttribute("initialPage");
+    " Ungunvvdees sondgoi ungiig oloh yostoi.\n\nStart darj togloomiig ehlvvlene vv!";
+  startDiv.setAttribute("class", "initialPage");
   startButton.addEventListener("click", start);
+  startDiv.appendChild(tutorial);
   startDiv.appendChild(startButton);
   root.appendChild(startDiv);
 }
 initial();
 
-function start() {}
+function start() {
+  root.innerHTML = "";
+  root.setAttribute("class", "gamePage");
+
+  let gameInfoDiv = document.createElement("div");
+  gameInfoDiv.setAttribute("class", "gameInfodiv");
+
+  let levelNum = 1;
+  let levelShow = document.createElement("input");
+  levelShow.innerText = `Level: ${levelNum}`;
+
+  currentScore = 0;
+  currentScoreShow = document.createElement("input");
+  currentScore.innerText = `Score: ${currentScore}`;
+
+  chance = 3;
+  chanceShow.document.createElement("input");
+  chanceShow.innerText = `Chance: ${chance}`;
+
+  gameInfoDiv.appendChild(levelShow);
+  gameInfoDiv.appendChild(currentScore);
+  gameInfoDiv.appendChild(chance);
+  root.appendChild(gameInfoDiv);
+}
